ff67d0297fa8af8cb0c70006dfc6a083aaf22253
Lia Skalkos <lskalkos@boomboxinc.com>
2015-03-19 11:16:01 -0400

    1: First commit.

---
+++ script.js
@@ -0,0 +1,296 @@
+
+//Tic_Tac_Toe serves as a namespace(?) 
+var Tic_Tac_Toe = {
+
+	/*******************|
+	|**COMPUTER OBJECT**|
+	|*******************/
+
+	Computer: function(opponent) {
+
+		this.opponent = opponent;
+		this.name = 'Computer';
+		this.turn = false;
+		this.piece = '';
+		this.move = function(boxes, scores) {
+
+			if(this.opponent.piece === "X") {
+				this.piece = "O";
+			} else if (this.opponent.piece === "O") {
+				this.piece = "X";
+			} else {
+				this.piece = "X";
+			};
+
+			var random_box = Math.floor(boxes.length * Math.random());
+			var move = boxes[random_box];
+			move.toString();
+
+			var index = boxes.indexOf(move);
+			if (index > -1) {
+			    boxes.splice(index, 1);
+			};
+
+			scores[move] = this.piece;
+
+			var elem = $("#" + move);
+			elem.val(this.piece).attr('disabled','');
+
+		};
+
+	},//Computer
+
+	/***************|
+	|**GAME OBJECT**|
+	|***************/
+
+	Game: function() {
+
+		//Set global game data
+		var $input = $('input'); //Collect all the DOM box inputs
+
+		var pieces = ['x','o', 'X', 'O']; //Define board pieces
+		var turns = 0; //Set number of turns taken
+		var scores = {}; //Create key-value pair object to hold each box's value, X or O
+		var game_set = false; //Game starts with no one having won yet
+		var boxes = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]; //Array holding all currently available boxes
+		var next_player = null; //Initialize a next player variable 		
+		var players = []; //Array to hold all players
+		var entry = null; //Initialize a box entry variable
+		var player1 = null; //Initialize player 1
+		var player2 = null; //Initialize player 2
+
+		//Function to check if a player has won
+		function checkWins(player) {
+
+			var scores_array = Object.keys(scores);
+
+			//Defines what counts as a win with conditional statements and booleans
+			var diagnol_win1 = scores["e"] == scores["i"] && scores["e"]==scores["a"] && scores["e"] != undefined || false || null;
+			var diagnol_win2 = scores["e"] == scores["c"] && scores["e"]==scores["g"] && scores["e"] != undefined || false || null;
+			var horizontal_win1 = scores["a"] == scores["c"] && scores["a"]==scores["b"] && scores["a"] != undefined || false || null;				
+			var horizontal_win2 = scores["d"] == scores["f"] && scores["d"]==scores["e"] && scores["d"] != undefined || false || null;	
+			var horizontal_win3 = scores["g"] == scores["i"] && scores["g"]==scores["h"] && scores["g"] != undefined || false || null;	
+			var vertical_win1 = scores["a"] == scores["g"] && scores["a"]==scores["d"] && scores["a"] != undefined || false || null;	
+			var vertical_win2 = scores["b"] == scores["h"] && scores["b"]==scores["e"] && scores["b"] != undefined || false || null;	
+			var vertical_win3 = scores["c"] == scores["i"] && scores["c"]==scores["f"] && scores["c"] != undefined || false || null;	
+
+			//Detect a win
+			if (diagnol_win1 || diagnol_win2 || horizontal_win1 || horizontal_win2 || horizontal_win3 || vertical_win1 || vertical_win2 || vertical_win3) {
+				alert(player.name + " Won! Game set.");
+				$input.val('').attr('disabled', '');
+				game_set = true;
+				location.reload(true);
+				return false;
+			}
+
+			//Detect a tie
+			if(scores_array.length === 9) {
+				alert("It's a tie!")
+				$input.val('').attr('disabled', '');
+				game_set = true;
+				location.reload(true);	
+				return false; 			
+			}
+
+			return false;
+
+
+		};//checkWins
+
+		//Function to check that a player is using the right piece
+		function checkPlayerPiece() {
+
+			if(turns === 0) {
+				if(jQuery.inArray(entry, pieces) === -1) {
+					alert("Please use an 'X' or an 'O' as your piece!");
+					return false;
+				};				
+			};
+
+			if (turns > 0) {
+				if(player1.turn == true && entry == player2.piece || player1.turn == true && jQuery.inArray(entry, pieces) === -1) {
+					if(player2.piece == 'X') {
+						alert("You must use an O.");
+						return false;
+					} else {
+						alert("You must use an X.");
+						return false;
+					};
+				} else if (player2.turn == true && entry == player1.piece || player2.turn == true && jQuery.inArray(entry, pieces) === -1) {
+					if(player1.piece == 'X') {
+						alert("You must use an O.");
+						return false;
+					} else {
+						alert("You must use an X.");
+						return false;
+					};				
+				};
+			};
+
+			return true;
+
+		};//checkPlayerPiece
+
+		//Function to execute a human player move
+		function humanMove(computer_opponent_true, player1, player2) {
+
+			$input.keyup(function(e) {
+
+				if(e.keyCode == 13) {
+					entry = $(this).val().toUpperCase();
+
+					if(player1.turn == true) {
+						player1.piece = entry;
+					} else if(!computer_opponent_true && player2.turn == true) {
+						player2.piece = entry;
+					};
+
+					if(!checkPlayerPiece()) {
+						return false;
+					} else {
+
+						if(player1.turn == true) {
+							player1.piece = entry;
+						} else if(!computer_opponent_true && player2.turn == true) {
+							player2.piece = entry;
+						};
+ 
+						turns = 1 + turns;
+
+						var box = $(this).attr('id');
+
+						scores[box] = entry;
+
+						if(computer_opponent_true) {
+							var index = boxes.indexOf(box);
+							if (index > -1) {
+							    boxes.splice(index, 1);
+							};							
+						};
+
+						if(turns >= 5) {
+							checkWins(next_player);
+						};
+
+						if(!game_set) {
+
+							$(this).attr('disabled', '');
+
+							
+							if(player1.turn === true) {
+								player1.turn = false; 
+								player2.turn = true;
+								next_player = player2;
+								alert("Now it's " + next_player.name + "'s turn! " );
+								
+								if(computer_opponent_true) {
+									player2.move(boxes, scores);
+									turns = turns + 1;
+									if(turns >= 5) {
+										checkWins(next_player);
+									};
+									if(!game_set) {
+										player1.turn = true; 
+										player2.turn = false;
+										next_player = player1;
+										alert("Now it's " + player1.name + "'s turn! ");										
+									};
+								};
+							} else {
+								player1.turn = true;
+								player2.turn = false;
+								next_player = player1;
+								alert("Now it's " + next_player.name + "'s turn! " );
+							};															
+							
+						} else {
+							return false;
+						};
+
+					};
+
+				};
+				// };
+			})//keyup
+		};//humanMove
+
+		//Method to set up a human-human player game
+		this.twoPlayerGame = function() {
+
+			player1 = new Tic_Tac_Toe.Player();
+			player2 = new Tic_Tac_Toe.Player();
+			players = [player1, player2];
+
+			player1.name = prompt("Hello there. Please enter the first player's name:");
+			player2.name = prompt("Great. Please enter the second player's name:");
+
+			player1.turn = true;
+			next_player = player1;
+
+			humanMove(false, player1, player2);
+
+		}, //twoPlayerGame
+
+		//Method to set up a computer-human game
+		this.singlePlayerGame = function() {
+
+			player1 = new Tic_Tac_Toe.Player();
+			player1.name = prompt("Hello there. Please enter your name:");
+			
+			player2 = new Tic_Tac_Toe.Computer(player1);
+			players = [player1, player2];
+			var first_player = confirm("Computer go first?");
+
+			if(first_player === true) {
+				next_player = player2;
+				player2.move(boxes, scores);
+				turns = turns + 1;
+			}; 
+
+			player1.turn = true;
+			player2.turn = false;
+			next_player = player1;	
+
+			humanMove(true, player1, player2);	
+
+		}//singlePlayerGame
+
+
+	},//Game
+
+	/*****************|
+	|**PLAYER OBJECT**|
+	|*****************/
+
+	Player: function() {
+		this.name = '';
+		this.turn = false;
+		this.piece = ''		
+	}//Player
+
+};//Tic_Tac_Toe
+
+
+
+//When the DOM is ready load game functions
+$(document).ready(function() {
+
+	$('input').attr('disabled','');
+
+	$('#computer').click(function() {
+		var game = new Tic_Tac_Toe.Game();
+		game.singlePlayerGame();
+		$('input').removeAttr('disabled');		
+	});
+
+	$('#person').click(function() {
+		var game = new Tic_Tac_Toe.Game();
+		game.twoPlayerGame();
+		$('input').removeAttr('disabled');		
+	});	
+
+});
+
+
+
